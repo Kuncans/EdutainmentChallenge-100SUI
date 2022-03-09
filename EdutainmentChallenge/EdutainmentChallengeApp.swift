fd94b9dc4f97276e8cb4fc377f61941fd2ab827c
Kuncans <akafauna@gmail.com>
2022-03-09 10:23:16 +0000

    Initial Commit

---
+++ EdutainmentChallenge/EdutainmentChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentChallengeApp.swift
+//  EdutainmentChallenge
+//
+//  Created by Duncan Kent on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
